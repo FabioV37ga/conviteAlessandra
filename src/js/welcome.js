6ff5e61661dda5719d523f35a1adc7ee26e6806a
Fábio Veiga <veigabfabio@hotmail.com>
2025-03-23 19:16:17 -0300

    Welcome 'pronto'

--- src/js/welcome.js
+++ src/js/welcome.js
@@ -1,4 +1,14 @@
+var closeButton = document.querySelector(".welcome-close")
+closeButton.addEventListener("click", () => {
+    document.querySelector(".texto").classList.add('fadeout')
+    var squares = document.querySelectorAll(".square")
+    for (let i = 0; i <= squares.length - 1; i++){
+        squares[i].classList.add(`throwOut${i}`)
+    }
+})
+
+
 var welcomeWindow = document.querySelector(".window")
-welcomeWindow.children[2].addEventListener("animationend", ()=>{
+welcomeWindow.children[2].addEventListener("animationend", () => {
     welcomeWindow.remove()
 })
